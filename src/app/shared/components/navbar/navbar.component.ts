41ce74a63dc953357ae9f9cf2fe432da0af66cb8
Josué Amador <54346349+rodamaj@users.noreply.github.com>
2021-11-21 20:13:43 -0600

    Correcciones en login.

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -19,4 +19,8 @@ export class NavbarComponent implements OnInit {
   logout(): void {
     this.userService.performLogout();
   }
+
+  isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
+  }
 }
