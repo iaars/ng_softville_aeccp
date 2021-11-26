6ffddef63b9987621607c994ec03c76840de8ecd
Josué Amador <54346349+rodamaj@users.noreply.github.com>
2021-11-26 17:16:23 -0600

    Routes updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,13 @@ export class AppComponent {
 
   constructor(private router: Router) {}
 
-  goToDashboard(): void {
+  goToHome(): void {
     this.router.navigate(['/home']);
     this.opened = false;
   }
 
-  goToTagManager(): void {
-    this.router.navigate(['/tag-manager']);
-    this.opened = false;
-  }
-
-  goToOrdersHistory(): void {
-    this.router.navigate(['/orders-history']);
-    this.opened = false;
-  }
-  goToChangePassword(): void {
-    this.router.navigate(['/change-password']);
+  goToReports(): void {
+    this.router.navigate(['/home']);
     this.opened = false;
   }
 }
